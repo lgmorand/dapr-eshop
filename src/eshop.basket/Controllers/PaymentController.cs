b018bb52badeb89a73e26b6ef6e7c54557ec1150
Louis-Guillaume MORAND <lgm@lgmorand.com>
2020-05-18 13:58:58 +0200

    test

---
+++ src/eshop.basket/Controllers/PaymentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace eshop.basket.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        private readonly ILogger<PaymentController> _logger;
+
+        public PaymentController(ILogger<PaymentController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public bool Pay()
+        {
+            _logger.Log(LogLevel.Information, "Payment successful");
+            return true;
+        }
+    }
+}
